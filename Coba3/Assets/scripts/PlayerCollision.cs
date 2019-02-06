64b3fdab1e6f905b89fdba94a33851d6b96b2981
miketoreno97 <34428606+miketoreno97@users.noreply.github.com>
2019-02-06 16:45:07 +0700

    First commit
    unity project ke github

---
+++ Coba3/Assets/scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public playermovement Movement;
+    public GameManager gameManager;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "rintangan")
+        {
+            Movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+    }
+}
---
+++ Coba3/Assets/scripts/playermovement.cs
@@ -0,0 +1,54 @@
+﻿
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    public float jumpForce = 1000f;
+
+    public bool isgrounded;
+
+    // Update is called once per frame
+    void OnCollisionStay()
+    {
+        isgrounded = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("space") && isgrounded) 
+        {
+            rb.AddForce(0, jumpForce * Time.deltaTime, 0);
+            isgrounded = false;
+        }
+
+        if (rb.position.y < -2f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
